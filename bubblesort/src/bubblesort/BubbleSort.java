d721de0b2aa72c322621030d3c4eb4a411951b94
halo <halo@163.com>
2023-03-09 19:35:14 +0800

    1.0

--- bubblesort/src/bubblesort/BubbleSort.java
+++ bubblesort/src/bubblesort/BubbleSort.java
@@ -1,14 +1,15 @@
 package bubblesort;
 
+
 import java.util.Arrays;
 
 public class BubbleSort {
 	public static void main(String[] args) {
 		int[] values = {5,1,7,4,3,6,9,8,2};
 		System.out.println(Arrays.toString(values));
-		System.out.println(Arrays.toString(bubblesort(values)));
+		System.out.println(Arrays.toString(bubbleSort(values)));
 	}
-	static int[] bubblesort(int[] b){
+	static int[] bubbleSort(int[] b){
 		int temp;
 		for (int j = b.length; j > 0; j--) {
 			for (int i = 0; i < j-1; i++) {
@@ -21,4 +22,5 @@ public class BubbleSort {
 		}
 		return b;
 	}
+	//代码注释
 }
